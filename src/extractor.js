80319bb5b51cd2ab29119794946c2cc7c81ad958
Konstantin Pribluda <konstantin.pribluda@gmail.com>
2023-01-30 14:10:28 +0200

    script served from local web server

---
+++ src/extractor.js
@@ -0,0 +1,19 @@
+// ==UserScript==
+// @name         XingExtractor
+// @namespace    http://www.pribluda.de/
+// @version      0.1
+// @description  extract contact data from xing
+// @include https://www.xing.com/notifications/contacts
+// @author       Konstantin Pribluda
+// @grant        none
+// ==/UserScript==
+
+
+(function () {
+
+
+    const initGui = function() {
+        console.log('GUI initialised');
+    }
+    console.log('called on xing contact page');
+})();
