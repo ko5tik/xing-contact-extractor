59ef90eb46b66be4395a594e3f622bbe7c17f39b
Konstantin Pribluda <konstantin.pribluda@gmail.com>
2023-02-08 10:17:33 +0200

    some more improvements

--- src/extractor.js
+++ src/extractor.js
@@ -13,16 +13,24 @@
 
 (function () {
 
-    const extractXINGContacts = '_extract_XING_contacts';
+    const xingContactPage = 'https://www.xing.com/notifications/contacts';
+    const doExtractXINGContacts = '_extract_XING_contacts';
+    const extractedContacts = '_XING_contacts';
 
     function GM_onMessage(label, callback) {
         GM_addValueChangeListener(label, function () {
+            console.log('received: ', label, ' => ', arguments[2])
             callback.apply(undefined, arguments[2]);
         });
     }
 
     function GM_sendMessage(label) {
+        console.log('send:', label, ' => ', Array.from(arguments).slice(1))
         GM_setValue(label, Array.from(arguments).slice(1));
+    }
+
+    const processContacts = function (src, contacts) {
+        console.log('[LI] ', src, '=>', contacts)
     }
 
     /**
@@ -30,20 +38,30 @@
      */
     const loadContacts = function () {
 
-
-        // asynchronously scroll  until element is not there anymore
+        // asynchronously scroll  until element is not there anymore.
+        //  then send message and terminate himself
         const toHandler = function () {
             // do some stuff
             let elem = $("*[data-qa=\"lazy-loader-loading-indicator\"]").get(0);
             if (elem) {
                 //  element is there, scroll
                 elem.scrollIntoView();
-                console.log('scrolling....');
+                console.log('[XING] scrolling....');
                 setTimeout(arguments.callee, 1000);
             } else {
                 console.log('got all entries,  retrieve')
-                const values = $("li[data-xds='ListItem'] div:not(.ceLLDS) >h2[data-xds='Headline']").map(function(){ return $(this).text()})
+                values = $("li[data-xds='ListItem'] div:not(.ceLLDS) >h2[data-xds='Headline']").map(function () {
+                    return $(this).text()
+                }).get()
+
+                //values.push(Date.now())
+
                 console.log(values)
+                GM_sendMessage(extractedContacts, values)
+                console.log('[XING] message sent')
+                //  and close  himself
+                window.close();
+                console.log('[XING] window closed')
             }
         };
 
@@ -52,26 +70,31 @@
     }
 
     const initLinkedIn = function () {
-        console.log('GUI initialised');
+        console.log('[LI] init GUI');
 
-        //  create iframe for XING contacts page
-        //const iframe = document.body.appendChild(document.createElement('iframe'));
-        //iframe.style.display = 'none';
-        //iframe.src = 'https://www.xing.com/notifications/contacts';
+        console.log('current;', GM_getValue(extractedContacts));
+        GM_setValue(extractedContacts, '');
 
-        //console.log('iframe created');
+        GM_onMessage(extractedContacts, function (src, message) {
+            console.log('[LI] contact data recieved' + message);
+            processContacts(src, message);
+        });
 
+        console.log('[LI] listener in place')
         // initialise button to start the prrocess
         var button = document.createElement("Button");
         button.innerHTML = "Import";
         button.style = "top:0;left:0;position:absolute;z-index: 9999"
         button.onclick = () => {
-            console.log("clicked!!!!!");
-            GM_sendMessage(extractXINGContacts, Date.now())
-            console.log('message sent');
+            console.log("[LI] clicked!!!!!");
+
+            // initialise value with 20 seconds in the future. this lag ought to be enough
+            //  even in germany
+            GM_setValue(doExtractXINGContacts, Date.now() + 20000)
+            console.log('[LI] message sent');
+            GM_openInTab(xingContactPage, {active: true});
         };
         document.body.appendChild(button);
-
     }
 
 
@@ -81,13 +104,17 @@
         console.log('on linkedin contact page')
         initLinkedIn();
     } else if (window.location.href === 'https://www.xing.com/notifications/contacts') {
-        console.log('on xing contact page. init message');
-        GM_onMessage(extractXINGContacts, function (src, message) {
-            console.log('[xing] load contact triggered' + message);
-            loadContacts();
-        });
+        console.log('[XING] on xing contact page. request is present:', GM_getValue(doExtractXINGContacts));
 
-        console.log('channel on message created')
+
+        // is there active request to import contacts,  and time is not elapsed
+        if (GM_getValue(doExtractXINGContacts) > Date.now()) {
+            console.log('[XING]  extracting');
+            GM_setValue(doExtractXINGContacts, '');
+            loadContacts();
+        }
+
+
     }
 
 })
